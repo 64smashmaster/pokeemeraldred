ad2283328e27aeb85dadb208ddf7bcaf4ef3a24b
64smashmaster <74997498+64smashmaster@users.noreply.github.com>
2022-02-11 22:37:18 -0500

    Initialize casino_mart

---
+++ include/coins.h
@@ -0,0 +1,13 @@
+#ifndef GUARD_COINS_H
+#define GUARD_COINS_H
+
+void PrintCoinsString(u32 coinAmount);
+void ShowCoinsWindow(u32 coinAmount, u8 x, u8 y);
+void HideCoinsWindow(void);
+u16 GetCoins(void);
+void SetCoins(u16 coinAmount);
+bool8 AddCoins(u16 toAdd);
+bool8 RemoveCoins(u16 toSub);
+bool8 IsEnoughCoins(u16 cost);
+
+#endif // GUARD_COINS_H
