150b7e47f04c90440be9a70318d342586fc508b5
64smashmaster <74997498+64smashmaster@users.noreply.github.com>
2022-02-11 22:46:22 -0500

    Initialize trainer_see_scripts

---
+++ include/constants/trainer_types.h
@@ -0,0 +1,10 @@
+#ifndef GUARD_CONSTANTS_TRAINER_TYPES_H
+#define GUARD_CONSTANTS_TRAINER_TYPES_H
+
+#define TRAINER_TYPE_NONE               0
+#define TRAINER_TYPE_NORMAL             1
+#define TRAINER_TYPE_SEE_ALL_DIRECTIONS 2
+#define TRAINER_TYPE_BURIED             3
+#define TRAINER_TYPE_RUN_SCRIPT         4   //anything larger than this is a flag for running the script
+
+#endif  // GUARD_CONSTANTS_TRAINER_TYPES_H
