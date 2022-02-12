1d79bebfc791042e63e4f448a3121ec42f2364a5
64smashmaster <74997498+64smashmaster@users.noreply.github.com>
2022-02-11 22:56:46 -0500

    Initialize ghoul_expansion

---
+++ include/constants/ghoul_config.h
@@ -0,0 +1,9 @@
+#ifndef GUARD_CONSTANTS_GHOUL_CONFIG_H
+#define GUARD_CONSTANTS_GHOUL_CONFIG_H
+
+#define SLOW_MOVEMENT_ON_STAIRS             TRUE
+#define FOLLOW_ME_IMPLEMENTED               TRUE    //for stairs movement. see ObjectMovingOnRockStairs in src/field_player_avatar.c
+#define SIDEWAYS_STAIRS_IMPLEMENTED         TRUE
+#define POST_BATTLE_FOLLOWER_FIX            FALSE   //if you experience the follower de-syncing with the player after battle, set to TRUE
+
+#endif // GUARD_CONSTANTS_GHOUL_CONFIG_H
